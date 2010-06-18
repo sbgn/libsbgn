0ec306c34417f5bb0dfbcddd4b7239d76b3b8a6d
amarillion <amarillion@users.noreply.github.com>
2010-06-18 21:43:32 +0000

    Fixed classpath for junit test

--- org.sbgn/test/org/sbgn/bindings/Test.java
+++ org.sbgn/test/org/sbgn/bindings/Test.java
@@ -10,7 +10,7 @@ import junit.framework.TestCase;
 
 public class Test extends TestCase
 {
-	static File file1 = new File ("../test-files/adh.xml");
+	static File file1 = new File ("test-files/adh.xml");
 	
 	public static void test1() throws JAXBException
 	{
