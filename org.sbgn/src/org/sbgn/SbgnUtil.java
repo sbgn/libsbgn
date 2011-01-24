4d5fa6c42c32f6b4c322f1db5f2f441e60bb5aa4
amarillion <amarillion@users.noreply.github.com>
2011-01-24 22:23:53 +0000

    Created validation example

--- org.sbgn/src/org/sbgn/SbgnUtil.java
+++ org.sbgn/src/org/sbgn/SbgnUtil.java
@@ -2,12 +2,16 @@ package org.sbgn;
 
 import java.io.File;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Schema;
 
 import org.sbgn.bindings.Sbgn;
+import org.xml.sax.SAXException;
 
 public class SbgnUtil
 {
@@ -19,6 +23,32 @@ public class SbgnUtil
 		
 		// Now read from "f" and put the result in "sbgn"
 		Sbgn result = (Sbgn)unmarshaller.unmarshal (f);
+		return result;
+	}
+	
+	public static boolean isValid (File f, File xsd)
+	{
+		boolean result = true;
+		try
+		{
+			Schema schema;
+			SchemaFactory schemaFactory = SchemaFactory.newInstance( XMLConstants.W3C_XML_SCHEMA_NS_URI );
+			schema = schemaFactory.newSchema(xsd);
+			JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			
+			unmarshaller.setSchema(schema);
+			
+			unmarshaller.unmarshal (f);
+		}
+		catch (SAXException e)
+		{
+			result = false;
+		}
+		catch (JAXBException e)
+		{
+			result = false;
+		}
 		return result;
 	}
 	
