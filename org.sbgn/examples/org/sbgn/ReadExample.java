07124831f373c5c4281653799de23ab726252040
amarillion <amarillion@users.noreply.github.com>
2011-01-24 21:42:21 +0000

    added some java examples

---
+++ org.sbgn/examples/org/sbgn/ReadExample.java
@@ -0,0 +1,51 @@
+package org.sbgn;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import org.sbgn.SbgnUtil;
+import org.sbgn.bindings.Arc;
+import org.sbgn.bindings.Glyph;
+import org.sbgn.bindings.Map;
+import org.sbgn.bindings.Sbgn;
+
+public class ReadExample
+{
+	public static void main(String[] args) throws JAXBException
+	{
+		// our sbgnml file goes in "f"
+		File f = new File ("../test-files/adh.sbgn");
+				
+		// Now read from "f" and put the result in "sbgn"
+		Sbgn sbgn = SbgnUtil.readFromFile(f);
+
+		// an sbgn file may consist of multiple maps.
+		// we can access a list of them with getMap() 
+		for (Map map : sbgn.getMap())
+		{
+			System.out.println ("Begin of Map");
+
+			// we can get a list of glyphs (nodes) in this map with getGlyph()
+			for (Glyph g : map.getGlyph())
+			{
+				// print the sbgn class of this glyph
+				System.out.print (" Glyph with class " + g.getId());
+				
+				// if there is a label, print it as well
+				if (g.getLabel() != null)
+					System.out.println (", and label " + g.getLabel().getText());
+				else
+					System.out.println (", without label");
+			}
+
+			// we can get a list of arcs (edges) in this map with getArc()
+			for (Arc a : map.getArc())
+			{
+				// print the class of this arc
+				System.out.println (" Arc with class " + a.getClazz());
+			}
+		
+		}
+	}
+}
