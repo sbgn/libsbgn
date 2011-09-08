c1b16f8c4dc5a1ead08eca9bbde175390fc03bab
amarillion <amarillion@94e7ea51-778a-49e2-a14f-81f7d0c006ea>
2011-09-08 17:08:18 +0000

    Added utility function and example for schematron validation

--- trunk/org.sbgn/examples/org/sbgn/ValidationExample.java
+++ trunk/org.sbgn/examples/org/sbgn/ValidationExample.java
@@ -1,20 +1,32 @@
 package org.sbgn;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import javax.xml.bind.JAXBException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 
-import org.sbgn.bindings.Sbgn;
+import org.sbgn.schematron.Issue;
+import org.sbgn.schematron.SchematronValidator;
 import org.xml.sax.SAXException;
 
+/**
+ * This is an example that shows both low-level validation using XML Schema, 
+ * and high-level validation using schematron.
+ */
 public class ValidationExample
 {
-
-	public static void main (String[] args) throws JAXBException, SAXException
+	/**
+	 * Low level validation using XML Schema.
+	 * This will check if the XML is properly structured.
+	 * @throws SAXException 
+	 * @throws JAXBException 
+	 */
+	public static void lowLevelExample(File f) throws JAXBException, SAXException
 	{
 		File xsd = new File ("../resources/SBGN.xsd");
-		File f = new File ("../test-files/adh.sbgn");
-		
 		boolean isValid = SbgnUtil.isValid(f, xsd);
 		
 		if (isValid)
@@ -22,5 +34,36 @@ public class ValidationExample
 		else
 			System.out.println ("Validation failed");
 	}
+	
+	/**
+	 * High-level validation using schematron.
+	 * This will check if the drawing rules of SBGN are fulfilled. 
+	 * @throws SAXException 
+	 * @throws TransformerException 
+	 * @throws ParserConfigurationException 
+	 * @throws IOException 
+	 */
+	public static void highLevelExample(File f) throws IOException, ParserConfigurationException, TransformerException, SAXException
+	{
+		File schematron = new File ("../validation/rules/sbgn_pd.sch");		
+
+		// validation will result in a list of issues
+		List<Issue> issues = SchematronValidator.validate(f, schematron);
+
+		// print each issue individually.
+		System.out.println ("There are " + issues.size() + " validation problems"); 
+		for (Issue issue : issues)
+		{
+			System.out.println (issue);
+		}
+	}
+	
+	public static void main (String[] args) throws JAXBException, SAXException, ParserConfigurationException, IOException, TransformerException
+	{
+		File f = new File ("../test-files/PD/adh.sbgn");
+		lowLevelExample(f);
 		
+		File f2 = new File ("../test-files/PD/multimer2.sbgn");
+		highLevelExample (f2);			
+	}
 }
