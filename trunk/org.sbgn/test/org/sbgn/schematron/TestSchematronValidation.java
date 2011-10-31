b37a2afbe6298fcb34cfa1eb5740f66e4c56bedc
amarillion <amarillion@94e7ea51-778a-49e2-a14f-81f7d0c006ea>
2011-10-31 23:46:09 +0000

    Include schematron / xsd in jar file, and load them from classpath

--- trunk/org.sbgn/test/org/sbgn/schematron/TestSchematronValidation.java
+++ trunk/org.sbgn/test/org/sbgn/schematron/TestSchematronValidation.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
-import org.sbgn.SbgnUtil;
 import org.xml.sax.SAXException;
 
 import junit.framework.TestCase;
@@ -19,29 +18,14 @@ public class TestSchematronValidation extends TestCase
 	File rulesDir = new File("validation/rules/");
 	File testFilesBase = new File ("test-files");
 	
-	File fER = new File (rulesDir, "sbgn_er.sch");
-	File fPD = new File (rulesDir, "sbgn_pd.sch");
-	File fAF = new File (rulesDir, "sbgn_af.sch");
-
-	private Map<String, File> schematronByLang = new HashMap<String, File>();
-	
 	String[] langShortNames = new String[] { "PD", "ER", "AF" };
 	
-	public void setUp()
-	{
-		schematronByLang.put("AF", fAF);
-		schematronByLang.put("ER", fER);
-		schematronByLang.put("PD", fPD);
-	}
-		
 	public void testAllTestCases() throws IOException, ParserConfigurationException, TransformerException, SAXException
 	{
 		Map<File, List<Issue>> issuePerFile = new HashMap<File, List<Issue>>();
 		
 		for (String lang : langShortNames)
 		{
-			File sch = schematronByLang.get(lang);
-			assertTrue ("Could not find file " + sch, sch.exists());
 			File testFilesDir = new File (testFilesBase, lang);
 			assertTrue ("Could not find directory " + testFilesDir, testFilesDir.exists());
 			assertTrue (testFilesDir.isDirectory());
@@ -50,7 +34,7 @@ public class TestSchematronValidation extends TestCase
 			{
 				if (!f.getName().endsWith(".sbgn")) continue;
 				
-				List<Issue> issues = SchematronValidator.validate(f, sch);				
+				List<Issue> issues = SchematronValidator.validate(f);				
 				issuePerFile.put(f, issues);
 			}			
 		
