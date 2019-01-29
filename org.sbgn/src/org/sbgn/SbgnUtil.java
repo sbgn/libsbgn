0e2610abbb12338c3007def85f2294be7a9dba7f
Frank Bergmann <fbergman@caltech.edu>
2019-01-29 14:38:43 +0100

    - issue #39: added a bunch of overloads to readFrom / writeTo

--- org.sbgn/src/org/sbgn/SbgnUtil.java
+++ org.sbgn/src/org/sbgn/SbgnUtil.java
@@ -2,6 +2,10 @@ package org.sbgn;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.URL;
 
 import javax.xml.XMLConstants;
@@ -15,6 +19,8 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Schema;
 
 import org.sbgn.bindings.Sbgn;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class SbgnUtil
@@ -32,6 +38,82 @@ public class SbgnUtil
 		
 		// Now read from "f" and put the result in "sbgn"
 		Sbgn result = (Sbgn)unmarshaller.unmarshal (f);
+		return result;
+	}
+
+
+	/**
+	 * Read an sbgn file (without validating against the schema)
+	 * @param is input stream to read from 
+	 * @returns Sbgn data structure
+	 * @throws JAXBException if there is an IO error, or the file is not SBGNML.
+	 */
+	public static Sbgn readFrom (InputStream is) throws JAXBException
+	{
+		JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		
+		Sbgn result = (Sbgn)unmarshaller.unmarshal (is);
+		return result;
+	}
+
+	/**
+	 * Read an sbgn file (without validating against the schema)
+	 * @param r reader to read from 
+	 * @returns Sbgn data structure
+	 * @throws JAXBException if there is an IO error, or the file is not SBGNML.
+	 */
+	public static Sbgn readFrom (Reader r) throws JAXBException
+	{
+		JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		
+		Sbgn result = (Sbgn)unmarshaller.unmarshal (r);
+		return result;
+	}
+
+	/**
+	 * Read an sbgn file (without validating against the schema)
+	 * @param url url to read from 
+	 * @returns Sbgn data structure
+	 * @throws JAXBException if there is an IO error, or the file is not SBGNML.
+	 */
+	public static Sbgn readFrom (URL url) throws JAXBException
+	{
+		JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		
+		Sbgn result = (Sbgn)unmarshaller.unmarshal (url);
+		return result;
+	}
+
+	/**
+	 * Read an sbgn file (without validating against the schema)
+	 * @param source source to read from 
+	 * @returns Sbgn data structure
+	 * @throws JAXBException if there is an IO error, or the file is not SBGNML.
+	 */
+	public static Sbgn readFrom (InputSource source) throws JAXBException
+	{
+		JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		
+		Sbgn result = (Sbgn)unmarshaller.unmarshal (source);
+		return result;
+	}
+
+	/**
+	 * Read an sbgn file (without validating against the schema)
+	 * @param node node to read from 
+	 * @returns Sbgn data structure
+	 * @throws JAXBException if there is an IO error, or the file is not SBGNML.
+	 */
+	public static Sbgn readFrom (Node node) throws JAXBException
+	{
+		JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		
+		Sbgn result = (Sbgn)unmarshaller.unmarshal (node);
 		return result;
 	}
 
@@ -87,7 +169,7 @@ public class SbgnUtil
 	/**
 	 * Write sbgn to a file 
 	 * @param sbgn Sbgn data structure to write
-	 * @f output file
+	 * @param f output file
 	 * @throws JAXBException if there is an IO error
 	 */
 	public static void writeToFile (Sbgn sbgn, File f) throws JAXBException
@@ -98,6 +180,51 @@ public class SbgnUtil
 
 		// now write everything to disk
 		marshaller.marshal(sbgn, f);
+	}
+
+	/**
+	 * Write sbgn to a file 
+	 * @param sbgn Sbgn data structure to write
+	 * @param os output stream
+	 * @throws JAXBException if there is an IO error
+	 */
+	public static void writeTo (Sbgn sbgn, OutputStream os) throws JAXBException
+	{
+		JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
+
+		marshaller.marshal(sbgn, os);
+	}
+	
+	/**
+	 * Write sbgn to a file 
+	 * @param sbgn Sbgn data structure to write
+	 * @param w output writer
+	 * @throws JAXBException if there is an IO error
+	 */
+	public static void writeTo (Sbgn sbgn, Writer w) throws JAXBException
+	{
+		JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
+
+		marshaller.marshal(sbgn, w);
+	}
+
+	/**
+	 * Write sbgn to a file 
+	 * @param sbgn Sbgn data structure to write
+	 * @param node output node
+	 * @throws JAXBException if there is an IO error
+	 */
+	public static void writeTo (Sbgn sbgn, Node node) throws JAXBException
+	{
+		JAXBContext context = JAXBContext.newInstance("org.sbgn.bindings");
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
+
+		marshaller.marshal(sbgn, node);
 	}
 
 }
